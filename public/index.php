00df456483e0107f38a4331fdb21fdb033e2002a
Márcio <marciojr91@gmail.com>
2016-01-10 02:03:30 -0200

    Remoção do arquivo init_autoloader.php

--- public/index.php
+++ public/index.php
@@ -1,18 +1,26 @@
 <?php
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
 chdir(dirname(__DIR__));
 
+// if environment is development, then show all erros
 if (getenv('APP_ENV') == 'development') {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 }
 
-// Setup autoloading
-require 'init_autoloader.php';
+// Composer autoloading
+if (file_exists('./../vendor/autoload.php')) {
+    $loader = include './../vendor/autoload.php';
+}
+
+if (!class_exists('Zend\Loader\AutoloaderFactory')) {
+    throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install`.');
+}
 
 // Run the application!
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();
