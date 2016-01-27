575e561d278a466a75c37b7044b142ab1d85dcda
Gabriel Richard Pereira <rickardch@gmail.com>
2016-01-27 18:06:40 -0200

    Adaptado o visualizador de conteúdo .csv para a pré-entrevista do PSA 2016

--- module/Recruitment/src/Recruitment/Controller/CsvViewerController.php
+++ module/Recruitment/src/Recruitment/Controller/CsvViewerController.php
@@ -41,21 +41,96 @@ class CsvViewerController extends AbstractActionController
                     
                 if (($fileHandler = fopen($data['csv_file']['tmp_name'], "r")) 
                         !== FALSE) {
-                    while (($values = fgetcsv($fileHandler, 1000, ",")) !== FALSE) {
-                        $dates[] = $values[0];
-                        $names[] = $values[1];
-                        $mascots[] = $values[2];
-                        $games[] = explode(',', $values[3]);
-                        $compulsory[] = $values[4];
-                        $optional[] = $values[5];
+                    $title = fgetcsv($fileHandler, 10000, ",");
+                    while (($values = fgetcsv($fileHandler, 10000, ",")) !== FALSE) {
+                        $content[] = $values;
                     }
                     $info = array(
-                        'dates' => $dates,
-                        'names' => $names,
-                        'mascots' => $mascots,
-                        'games' => $games,
-                        'compulsory' => $compulsory,
-                        'optional' => $optional,
+                        'title' => $title,
+                        'content' => $content,
+                        'blocks' => array(
+                                array(
+                                    'boundary' => 3, 
+                                    'title' => 'Informações Pessoais',
+                                    'nextBoundary' => 11,
+                                ),
+                                array(
+                                    'boundary' => 11, 
+                                    'title' => 'Endereço',
+                                    'nextBoundary' => 15,
+                                ),
+                                array(
+                                    'boundary' => 15, 
+                                    'title' => 'Informações do responsável',
+                                    'nextBoundary' => 21,
+                                ),
+                                array(
+                                    'boundary' => 21, 
+                                    'title' => 'Educação Formal',
+                                    'nextBoundary' => 31,
+                                ),
+                                array(
+                                    'boundary' => 31, 
+                                    'title' => 'Moradia',
+                                    'nextBoundary' => 49,
+                                ),
+                                array(
+                                    'boundary' => 49, 
+                                    'title' => 'Outros Imóveis',
+                                    'nextBoundary' => 63,
+                                ),
+                                array(
+                                    'boundary' => 63, 
+                                    'title' => 'Bens móveis',
+                                    'nextBoundary' => 79,
+                                ),
+                                array(
+                                    'boundary' => 79, 
+                                    'title' => 'Você se considera (Etnia)',
+                                    'nextBoundary' => 80,
+                                ),
+                                array(
+                                    'boundary' => 80, 
+                                    'title' => 'Meio de Transporte',
+                                    'nextBoundary' => 83,
+                                ),
+                                array(
+                                    'boundary' => 83, 
+                                    'title' => 'Despesas',
+                                    'nextBoundary' => 90,
+                                ),
+                                array(
+                                    'boundary' => 90, 
+                                    'title' => 'Grupo Familiar',
+                                    'nextBoundary' => 164,
+                                ),
+                                array(
+                                    'boundary' => 164, 
+                                    'title' => 'Você é ex-aluno do CATS?',
+                                    'nextBoundary' => 165,
+                                ),
+                                array(
+                                    'boundary' => 165, 
+                                    'title' => 'Informe ou esclareça sobre dados '
+                                    . 'não contemplados neste formulário ou situações '
+                                    . 'especiais que julgar conveniente',
+                                    'nextBoundary' => 166,
+                                ),
+                                array(
+                                    'boundary' => 166, 
+                                    'title' => 'Declaro, para fins de direito, '
+                                    . 'sob as penas da lei, e em atendimento ao '
+                                    . 'EDITAL do CATS, que as informações constantes '
+                                    . 'dos documentos que apresento para a pré-entrevista '
+                                    . 'no processo seletivo de alunos são verdadeiras, '
+                                    . '(ou são fieis à verdade e condizentes com '
+                                    . 'a realidade dos fatos à época). Fico ciente '
+                                    . 'através desse documento que a falsidade dessa '
+                                    . 'declaração configura crime previsto no Código '
+                                    . 'Penal Brasileiro, e passível de apuração na forma da Lei.',
+                                    'nextBoundary' => 167,
+                                ),
+                        ),
                     );
                     fclose($fileHandler);
                 } else {
