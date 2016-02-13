c8d59aa63ebf1f6236db878713915889630f512e
Márcio <marciojr91@gmail.com>
2016-02-13 18:22:34 -0200

    layout do site passa carregar os scripts (js) utilizando o requireJs

---
+++ public/js/site-config.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+requirejs.config({
+    baseUrl: '/vendor',
+    paths: {
+        jquery: 'AdminLTE/plugins/jQuery/jQuery-2.1.4.min',
+        bootstrap: 'bootstrap/dist/js/bootstrap.min',
+        smoothscroll: 'smooth-scroll/dist/js/smooth-scroll.min',
+        app: '/js/app'
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery']
+        }
+    }
+});
+
+define(['smoothscroll', 'jquery', 'bootstrap'], function (smoothScroll) {
+    $(function () {
+        // needed to use bootstrap data-spy
+        $(window).trigger('load');
+
+        $('.wrapper').fadeIn('slow');
+        smoothScroll.init();
+    });
+});
