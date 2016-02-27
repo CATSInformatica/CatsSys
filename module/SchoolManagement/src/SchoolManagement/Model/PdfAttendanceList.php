1c7b201983bc67900ecb6636d6accecf388b2aed
Breno Silva <brenog.silva@hotmail.com>
2016-02-27 12:12:30 -0300

    Conclusão da lista de presença.

--- module/SchoolManagement/src/SchoolManagement/Model/PdfAttendanceList.php
+++ module/SchoolManagement/src/SchoolManagement/Model/PdfAttendanceList.php
@@ -15,21 +15,53 @@ use fpdf\FPDF;
  *
  * @author Breno Silva <brenog.silva@hotmail.com>
  */
-class PdfAttendanceList 
-{
+class PdfAttendanceList {
+
     const FONT = 'Arial';
-    
-    private $studentNames;
-    
-    public function __construct($names) 
-    {
-        $studentNames = $names;
+
+    private $students;
+
+    public function __construct($students = []) {
+        $this->students = $students;
+        //$this->dummyStudents();
     }
-    
-    public function generateList() 
-    {
-        $pdf = new FPDF('L', 'mm', 'A4');
+
+    protected function dummyStudents() {
+        for($i = 0; $i < 103; $i++){
+            $this->students[] = $this->students[0];
+        }
+    }
+
+    public function generateList() {
+        $pdf = new FPDF('P', 'mm', 'A4');
+        $pdf->addPage();
+        $pdf->setMargins(5, 5, 5);
+
+        $pdf->SetXY(5, 20);
+        $pdf->SetDrawColor(150);
         
+        $pdf->SetFont(self::FONT, 'B', 16);
+        $pdf->Cell(0, 10, utf8_decode("LISTA DE PRESENÇA"), '1', 1, 'C');
+        
+        $pdf->SetFont(self::FONT, 'B', 11);
+        $pdf->Cell(30, 5, "Matricula", '1', 0, 'C');
+        $pdf->Cell(80, 5, "Nome", '1', 0);
+        $pdf->Cell(22.5, 5, "__/__", '1', 0, 'C');
+        $pdf->Cell(22.5, 5, "__/__", '1', 0, 'C');
+        $pdf->Cell(22.5, 5, "__/__", '1', 0, 'C');
+        $pdf->Cell(22.5, 5, "__/__", '1', 1, 'C');
+
+        $pdf->SetFont(self::FONT,'', 9);
+        foreach ($this->students as $student) {
+            $pdf->Cell(30, 4, $student["id"], '1', 0, 'C');
+            $pdf->Cell(80, 4, utf8_decode($student["name"]), '1', 0);
+            $pdf->Cell(22.5, 4, "", '1', 0, 'C');
+            $pdf->Cell(22.5, 4, "", '1', 0, 'C');
+            $pdf->Cell(22.5, 4, "", '1', 0, 'C');
+            $pdf->Cell(22.5, 4, "", '1', 1, 'C');
+        }
+
         return $pdf->Output('Chamada', 'I', true);
     }
+
 }
