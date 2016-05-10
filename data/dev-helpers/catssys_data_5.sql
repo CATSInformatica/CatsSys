81216429ae379e257823c38b2c31e4e7d9a41f1b
Gabriel Richard Pereira <rickardch@gmail.com>
2016-05-09 21:47:28 -0300

    Correção do arquivo 'catssys_data_5.sql'.

--- data/dev-helpers/catssys_data_5.sql
+++ data/dev-helpers/catssys_data_5.sql
@@ -16,25 +16,13 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping data for table `cash_flow`
---
-
-LOCK TABLES `cash_flow` WRITE;
-/*!40000 ALTER TABLE `cash_flow` DISABLE KEYS */;
-INSERT INTO `cash_flow` VALUES (1,1,1,'2016-05-12',1000,'Descrição','Observação',1),(2,1,2,'2016-05-06',900,'Descrição','Observação',1),(3,1,1,'2016-05-14',700,'Descrição','Observação',2),(4,1,1,'2016-05-28',850,'Descrição','Observação',2),(5,1,2,'2016-05-28',850,'Descrição','Observação',2),(6,1,1,'2016-05-02',950,'Observação','Observação',3),(7,1,2,'2016-05-02',700,'Observação','cObservação',3),(8,1,1,'2016-05-04',600,'Observação','Observação',4),(9,1,2,'2016-05-28',900,'Observação','Observação',4),(10,1,1,'2016-05-01',789,'Observação','Observação',5),(11,1,2,'2016-02-02',456,'Observação','Observação',5),(12,1,1,'2016-05-06',598,'Observação','Observação',6),(13,1,2,'2016-05-12',486,'Observação','Observação',6),(14,1,2,'2016-05-04',610,'projectedExpenseData','projectedExpenseData',13),(15,1,1,'2016-05-12',298,'projectedExpenseData','projectedExpenseData',13),(16,1,1,'2016-05-13',698,'projectedExpenseData','projectedExpenseData',14),(17,1,2,'2016-06-02',523,'projectedExpenseData','projectedExpenseData',14),(18,2,1,'2016-05-12',333,'Descrição','Observação',17),(19,2,2,'2016-05-19',444,'Observação','Observação',17),(20,1,1,'2016-05-26',489,'Observação','Observação',18),(21,1,2,'2016-05-20',694,'Observação','Observação',18),(22,1,1,'2016-05-21',554,'monthlyBalanceOpen','monthlyBalanceOpen',26),(23,2,2,'2016-05-04',777,'monthlyBalanceOpen','monthlyBalanceOpen',26),(24,1,1,'2016-05-12',777,'monthlyBalanceOpen','monthlyBalanceOpen',27),(25,1,2,'2016-05-10',888,'monthlyBalanceOpen','monthlyBalanceOpen',27),(26,1,1,'2016-05-03',951,'monthlyBalanceOpen','monthlyBalanceOpen',28),(27,1,2,'2016-05-12',468,'monthlyBalanceOpen','monthlyBalanceOpen',28),(28,1,1,'2016-05-09',666,'\"paging\": false','\"paging\": false',29),(29,1,2,'2016-05-02',777,'Descrição','Descrição',29),(30,1,1,'2016-05-04',756,'Descrição','Descrição',30),(31,1,2,'2016-05-18',865,'Descrição','Descrição',30),(32,1,1,'2016-05-04',555,'Descrição','Descrição',31),(33,1,2,'2016-05-18',556,'Descrição','Descrição',31),(34,1,1,'2016-05-30',777,'Descrição','Descrição',32),(35,1,2,'2016-05-03',888,'Descrição','Descrição',32);
-/*!40000 ALTER TABLE `cash_flow` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Dumping data for table `cash_flow_type`
 --
 
 LOCK TABLES `cash_flow_type` WRITE;
 /*!40000 ALTER TABLE `cash_flow_type` DISABLE KEYS */;
-INSERT INTO `cash_flow_type` VALUES 
-(1,'COMIDA','COMIDA',0),
-(0, 'MENSALIDADE','Valor de entrada relativo à mensalidade paga por um aluno. Quando o valor for negativo indica que a mensalidade foi alterada ou removida. Este tipo de receita é inserida automaticamente por meio da funcionalidade de manipulação de mensalidades.', 1),
-(2,'DOAÇÃO','COMIDA',1);
+INSERT INTO `cash_flow_type` VALUES (1,'COMIDA','COMIDA',0),
+(0, 'MENSALIDADE','Valor de entrada relativo à mensalidade paga por um aluno. Quando o valor for negativo indica que a mensalidade foi alterada ou removida. Este tipo de receita é inserida automaticamente por meio da funcionalidade de manipulação de mensalidades.', 1),(2,'DOAÇÕES','DOAÇÕES',1);
 /*!40000 ALTER TABLE `cash_flow_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -44,8 +32,18 @@ UNLOCK TABLES;
 
 LOCK TABLES `monthly_balance` WRITE;
 /*!40000 ALTER TABLE `monthly_balance` DISABLE KEYS */;
-INSERT INTO `monthly_balance` VALUES (1,'2013-03-01','2016-05-07',900,800,900,1000,0,'Observação'),(2,'2015-09-01','2016-05-07',800,700,850,700,0,''),(3,'2016-03-01','2016-05-07',750,890,700,950,0,''),(4,'2015-11-01','2016-05-07',1000,500,900,600,0,''),(5,'2016-12-01','2016-05-07',610,987,456,789,0,''),(6,'2016-01-01','2016-05-07',687,459,486,598,0,''),(13,'2016-02-01','2016-05-07',500,360,610,298,0,''),(14,'2016-03-01','2016-05-07',600,800,523,698,0,''),(17,'2016-04-01','2016-05-07',500,400,444,333,0,''),(18,'2015-12-01','2016-05-07',666,777,694,489,0,''),(26,'2015-05-01','2016-05-07',455,655,777,554,0,''),(27,'2016-06-01','2016-05-07',999,888,888,777,0,''),(28,'2016-07-01','2016-05-07',456,654,468,951,0,''),(29,'2015-08-01','2016-05-07',777,666,777,666,0,''),(30,'2015-10-01','2016-05-07',456,852,865,756,0,''),(31,'2015-06-01','2016-05-07',555,555,556,555,0,''),(32,'2015-07-01','2016-05-07',999,999,888,777,0,''),(33,'2017-06-01',NULL,900,800,0,0,1,NULL);
+INSERT INTO `monthly_balance` VALUES (1,'2015-06-01','2016-05-09',1000,900,900,500,0,''),(2,'2016-05-01','2016-05-09',825,963,799,852,0,''),(3,'2015-07-01','2016-05-09',654,456,654,456,0,''),(4,'2015-08-01','2016-05-09',987,436,852,698,0,''),(5,'2015-09-05','2016-05-09',963,147,753,159,0,''),(6,'2015-10-03','2016-05-09',856,698,849,458,0,''),(7,'2015-11-06','2016-05-09',489,863,789,963,0,''),(8,'2015-12-05','2016-05-09',895,412,777,888,0,''),(9,'2016-01-01','2016-05-09',666,555,555,444,0,''),(10,'2016-02-26','2016-05-09',998,889,966,775,0,''),(11,'2016-03-24','2016-05-09',369,456,154,452,0,''),(12,'2016-05-12','2016-05-09',123,321,321,123,0,''),(13,'2016-04-22','2016-05-09',111,50,111,50,0,'');
 /*!40000 ALTER TABLE `monthly_balance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `cash_flow`
+--
+
+LOCK TABLES `cash_flow` WRITE;
+/*!40000 ALTER TABLE `cash_flow` DISABLE KEYS */;
+INSERT INTO `cash_flow` VALUES (1,1,1,1,'2016-05-09',500,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(2,1,2,1,'2016-05-07',900,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(3,1,1,2,'2016-05-09',852,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(4,1,2,2,'2016-05-09',799,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(5,1,1,3,'2016-05-09',456,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(6,1,2,3,'2016-05-09',654,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(7,1,2,4,'2016-05-09',852,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(8,1,1,4,'2016-05-12',698,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(9,1,1,5,'2016-05-09',159,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(10,1,2,5,'2016-05-09',753,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(11,1,1,6,'2016-05-09',458,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(12,1,2,6,'2016-05-09',849,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(13,1,1,7,'2016-05-09',963,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(14,1,2,7,'2016-05-09',789,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(15,1,1,8,'2016-05-09',888,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(16,1,2,8,'2016-05-09',777,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(17,1,1,9,'2016-05-09',444,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(18,1,2,9,'2016-05-09',555,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(19,1,1,10,'2016-05-09',775,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(20,1,2,10,'2016-05-09',966,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(21,1,1,11,'2016-05-09',452,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(22,1,2,11,'2016-05-09',154,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(23,1,1,12,'2016-05-09',123,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(24,1,2,12,'2016-05-21',321,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(25,1,1,13,'2016-05-09',50,'RECURSOS HUMANOS','RECURSOS HUMANOS'),(26,1,2,13,'2016-05-09',111,'RECURSOS HUMANOS','RECURSOS HUMANOS');
+/*!40000 ALTER TABLE `cash_flow` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -57,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-05-07 18:30:20
+-- Dump completed on 2016-05-09 21:33:14
